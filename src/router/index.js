7e79e66dd7fb0a9325054c400c88823d9573daf3
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-21 22:56:52 -0600

    add meta in routes and beforeEach

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -7,17 +8,20 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "Home" */ '../views/Home.vue')
+    component: () => import(/* webpackChunkName: "Home" */ '../views/Home.vue'),
+    meta: {requireAuth: true}
   },
   {
     path: '/shopping-cart',
     name: 'ShoppingCart',
-    component: () => import('../views/ShoppingCart.vue')
+    component: () => import('../views/ShoppingCart.vue'),
+    meta: {requireAuth: true}
   },
   {
     path: '/products-details',
     name: 'ProductsDetails',
-    component: () => import('../views/ProductsDetails.vue')
+    component: () => import('../views/ProductsDetails.vue'),
+    meta: {requireAuth: true}
   },
   {
     path: '/login',
@@ -30,6 +34,16 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const authenticated = to.matched.some(record => record.meta.requireAuth)
+  
+  if(authenticated && store.state.token === null){
+    next({name:'Login'})
+  }else{
+    next()
+  }
 })
 
 export default router

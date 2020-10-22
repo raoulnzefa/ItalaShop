f4dcf8a2d7bef4d609d649ef9c94b68101558c7e
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-21 22:55:37 -0600

    add function for get token in login

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,47 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import gql from 'graphql-tag'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: null
   },
   mutations: {
+    setToken(state,payload){
+      state.token = payload
+    }
   },
   actions: {
+    async login({commit},user){
+      const response = await this.$apollo.mutate({
+          mutation: gql`mutation($username: String!,
+            $password: String!){
+            login(input: {
+              username: $username,
+              password: $password
+            }){
+                token
+            }
+        }`,
+        variables:{
+          username: user.name,
+          password: user.password
+        }
+      })
+
+      console.log(response.data.token)
+      commit('setToken', response.data.token)
+      localStorage.setItem('token', response.data.token)
+    },
+    getToken({commit}){
+      const token = localStorage.getItem('token')
+
+      if(token){
+        commit('setToken',token)
+      }
+    }
   },
   modules: {
   }
